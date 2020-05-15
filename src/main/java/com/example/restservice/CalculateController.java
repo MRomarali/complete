d4035ecf320c00684676ddd6aa8a1413e137d684
OMAAB <omar.abdi.ali@ist.com>
2020-05-15 21:01:13 +0200

    Firts commit

---
+++ src/main/java/com/example/restservice/CalculateController.java
@@ -0,0 +1,25 @@
+package com.example.restservice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class CalculateController {
+
+	@CrossOrigin(origins = { "*" })
+	@GetMapping("/calculate")
+	public BarnRequestObject calculate() {
+		return new BarnRequestObject(123, 1, 2, 3);
+	}
+
+	@CrossOrigin(origins = { "*" })
+	@PostMapping("/calculate-sum")
+	public BarnResponseObject calculateSum(@RequestBody BarnRequestObject requestObject) {
+		Calculator calculator = new Calculator();
+		return calculator.calculate(requestObject);
+	}
+}
---
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -0,0 +1,24 @@
+package com.example.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/calculate").allowedOrigins("http://localhost:8081");
+            }
+        };
+    }
+}
